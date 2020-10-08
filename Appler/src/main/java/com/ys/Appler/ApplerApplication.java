bc146f74eb4c45a6b3272fa7393bcb4f44d9531f
dldydtn84 <63628253+dldydtn84@users.noreply.github.com>
2020-10-08 23:30:54 +0900

    IntelliJ upload
    spring boot 초기셋팅

---
+++ Appler/src/main/java/com/ys/Appler/ApplerApplication.java
@@ -0,0 +1,13 @@
+package com.ys.Appler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplerApplication.class, args);
+	}
+
+}
---
+++ Appler/src/test/java/com/ys/Appler/ApplerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.Appler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
