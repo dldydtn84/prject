ca6449bc337040ed28fffd44fb1e54cf8708a141
dldydtn84 <qjsro3975@naver.com>
2020-10-12 22:43:33 +0900

    thymeleaf setting

--- Appler/src/main/java/com/ys/Appler/controller/HomeController.java
+++ Appler/src/main/java/com/ys/Appler/controller/HomeController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("hello")
-    public String hello(Model model){
-        model.addAttribute("data","hello");
-        return "hello";
+    @GetMapping("index")
+    public String index(Model model){
+        //model.addAttribute("data","hello");
+        return "index";
     }
 
 
