2d814fc4995ecae3ac7933a619d8115978305d8a
dldydtn84 <qjsro3975@naver.com>
2020-11-26 02:40:59 +0900

    실시간 채팅 본인 구분

--- appler/src/main/java/com/ys/appler/handler/SockerHandler.java
+++ appler/src/main/java/com/ys/appler/handler/SockerHandler.java
@@ -1,72 +1,77 @@
 package com.ys.appler.handler;
 
-import com.ys.appler.config.auth.PrincipalDetails;
-import com.ys.appler.dto.MemberDto;
+
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import java.util.HashMap;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Component
 public class SockerHandler  extends TextWebSocketHandler {
 
-    private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
+    HashMap<String, WebSocketSession> sessionMap = new HashMap<>(); //웹소켓 세션을 담아둘 맵
 
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        //클라이언트가 서버로 메세지를 보냈을때의 서버 처리 로직
+    public void handleTextMessage(WebSocketSession session, TextMessage message) {
+        //메시지 발송
+        String msg = message.getPayload();
+        JSONObject obj = jsonToObjectParser(msg);
+        obj.put("userName",session.getPrincipal().getName());
 
-        //session으로부터 메세지를 받음
-        log.info("{}로 부터 {}받음 ", session.getPrincipal().getName(), message.getPayload());
-        String text = session.getPrincipal().getName()+" : "+message.getPayload();
+        System.out.println("obj ::"+obj);
 
-
-        for(WebSocketSession sess : sessionList){
-            sess.sendMessage(new TextMessage(text));
-            //세션을 principal 을 이용하여 불러온뒤 메세지는 전체전달한다.
+        for(String key : sessionMap.keySet()) {
+            WebSocketSession wss = sessionMap.get(key);
+            try {
+                wss.sendMessage(new TextMessage(obj.toJSONString()));
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
         }
-
-
-        super.handleTextMessage(session, message);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        //클라이언특가 만났을때 접속연결처리 로직
-
+        //소켓 연결
         super.afterConnectionEstablished(session);
 
-        //리스트를 이용하여 세션 주입
-        sessionList.add(session);
+        sessionMap.put(session.getId(), session);
+        JSONObject obj = new JSONObject();
+        obj.put("type", "getId");
+        obj.put("sessionId", session.getId());
+        session.sendMessage(new TextMessage(obj.toJSONString()));
 
-        log.info("{} 연결됨", session.getId());
-
-        log.info("채팅방 입장자 : "+ session.getPrincipal().getName());
-
+        System.out.println("sessionMap ::"+sessionMap);
+        System.out.println("obj2 ::"+obj);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        //클라이언트와 접속이 끊어졌을시 세션종료밑 퇴장 처리 로직
-
-        sessionList.remove(session);
-        //세션을 세션리스트에서 삭제한다.
-
-        log.info("{} 연결 끊김 " + session.getId());
-
-        log.info("채팅방 퇴장자 : "+ session.getPrincipal().getName());
-        //Principal 을 이용하여 세션의 이름을 가지고온다.
-
-
+        //소켓 종료
+        sessionMap.remove(session.getId());
         super.afterConnectionClosed(session, status);
     }
+
+    private static JSONObject jsonToObjectParser(String jsonStr) {
+        System.out.println("jsonStr ::"+jsonStr);
+        JSONParser parser = new JSONParser();
+        JSONObject obj = null;
+        try {
+            obj = (JSONObject) parser.parse(jsonStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return obj;
+    }
+
 }
